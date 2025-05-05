2861dfca75720b92ed55daac4a00d945996b5aa0
Ivan Repusic <repusicivan2@gmail.com>
2025-05-05 20:42:58 +0200

    12_calculator Exercise done!

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((sum, current) => sum * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0){
+    return 1;
+  } 
+  else {
+    let b = 1;
+    for (let i = 1; i <= a; i++){
+      b = b * i;
+    }
+    return b;
+  }
 };
 
 // Do not edit below this line
