d4b607c3cef3044076aafbdc18c82672cd5fff31
Ivan Repusic <repusicivan2@gmail.com>
2025-05-05 22:12:55 +0200

    14_ fibonacci Exercise Done!

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n == 0) return 0;
+    if (n < 0) return 'OOPS';
 
+    const arr = [0, 1];
+    for (let i = 2; i <= n; i++){
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr[n];
 };
 
 // Do not edit below this line
